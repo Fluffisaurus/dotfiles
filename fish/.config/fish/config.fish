b2d37a00d81b00d31bdcfb8975ec88e1d1fff713
Fluffisaurus <angushon96@gmail.com>
2025-08-26 12:52:54 -0700

    feat: add fisher and update fish config Update fish config to automatically install fisher and its plugins

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,6 +22,36 @@ if not type -q omf
     echo "Finished applying bobthefish settings!"
 end
 
+# Bootstrap Fisher package manager if not installed already
+if not type -q fisher
+    # Download fisher
+    echo "Fisher package manager is not installed."
+    echo "Downloading Fisher package manager..."
+    curl -sL https://git.io/fisher | source
+    echo "Finished installing Fisher!"
+
+    # Change XDG_CONFIG_HOME temporarily to prevent nonexistent commmands
+    # from being executed
+    set tmpdir (mktemp -d)
+    set xdg_config_home_backup $XDG_CONFIG_HOME
+    set -x XDG_CONFIG_HOME $tmpdir
+
+    # Install other plugins
+    fisher update
+
+    # Delete the temporary directory and restore XDG_CONFIG_HOME
+    rm -rf $tmpdir
+    set -gx XDG_CONFIG_HOME $xdg_config_home_backup
+end
+
+# Try updating the plugins
+if not set --query __dotfiles_fish_update
+    set -x __dotfiles_fish_update 1
+
+    diff -qw (cat $__fish_config_dir/fish_plugins | sort | psub) (fisher list | sort | psub) >/dev/null
+    or fisher update
+end
+
 # pipenv creates .venv inside each project, https://pipenv.pypa.io/en/latest/#basic-usage
 set -x PIPENV_VENV_IN_PROJECT true
 
