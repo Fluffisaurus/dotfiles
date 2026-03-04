c9c5a5bae77d919b42fbec724eb1a59babf59e6e
Fluffisaurus <angushon96@gmail.com>
2026-03-03 22:44:43 -0800

    feat: add light/dark theme auto color change for fish greeting

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,24 +1,36 @@
 function fish_greeting
-    set MAIN_COLOUR blue
-    set SECONDARY_COLOUR purple
-    set ACCENT_COLOUR cyan
+    set CURRENT_THEME $(gsettings get org.gnome.desktop.interface color-scheme)
 
-    set INFO_LAST_LOGIN $(echo 'Last login: '(set_color white)(last -F -n 1 -R $USER | head -n 1 | awk '{$1=$2=$3=$NF=""; print $0}' | xargs echo))
+    if test "$CURRENT_THEME" = "'prefer-dark'"
+        or test "$CURRENT_THEME" = "'default'"
+        set MAIN_COLOUR brblue
+        set SECONDARY_COLOUR brpurple
+        set ACCENT_COLOUR brcyan
+        set TEXT_COLOUR white
+    else
+        set MAIN_COLOUR blue
+        set SECONDARY_COLOUR purple
+        set ACCENT_COLOUR cyan
+        set TEXT_COLOUR black
+    end
 
-    set INFO_DATE $(echo 'Date:       '(set_color white)(date +"%A, %b %d, %Y"))
-    set INFO_TIME $(echo 'Time:       '(set_color white)(date +"%R, %Z %:z"))
-    set INFO_UPTIME $(echo 'Uptime:     '(set_color white)(uptime -p | sed 's/^[^up]*up //'))
+    set INFO_LAST_LOGIN $(echo 'Last login: '(set_color $TEXT_COLOUR)(last -F -n 1 -R $USER | head -n 1 | awk '{$1=$2=$3=$NF=""; print $0}' | xargs echo))
 
-    set INFO_HARDWARE $(echo 'Hardware:   '(set_color white)(hostnamectl | grep "Hardware Model: " | sed 's/[^:]*:[[:space:]]*//'))
-    set INFO_OS $(echo 'OS:         '(set_color white)(hostnamectl | grep "Operating System: " | sed 's/[^:]*:[[:space:]]*//'))
-    set INFO_VERSION $(echo 'Kernel:     '(set_color white)(hostnamectl | grep "Kernel: " | sed 's/[^:]*:[[:space:]]Linux *//'))
+    set INFO_DATE $(echo 'Date:       '(set_color $TEXT_COLOUR)(date +"%A, %b %d, %Y"))
+    set INFO_TIME $(echo 'Time:       '(set_color $TEXT_COLOUR)(date +"%R, %Z %:z"))
+    set INFO_UPTIME $(echo 'Uptime:     '(set_color $TEXT_COLOUR)(uptime -p | sed 's/^[^up]*up //'))
+
+    set INFO_HARDWARE $(echo 'Hardware:   '(set_color $TEXT_COLOUR)(hostnamectl | grep "Hardware Model: " | sed 's/[^:]*:[[:space:]]*//'))
+    set INFO_OS $(echo 'OS:         '(set_color $TEXT_COLOUR)(hostnamectl | grep "Operating System: " | sed 's/[^:]*:[[:space:]]*//'))
+    set INFO_VERSION $(echo 'Kernel:     '(set_color $TEXT_COLOUR)(hostnamectl | grep "Kernel: " | sed 's/[^:]*:[[:space:]]Linux *//'))
 
     set_color --dim --italics 808080
     echo -e $INFO_LAST_LOGIN '\n'
     set_color normal
 
-    echo 'Welcome back, '(set_color --bold $MAIN_COLOUR)(whoami)'@'(hostname)(set_color normal)(set_color white)', ya silly 🪿💨💨💨'
-    
+    set_color $TEXT_COLOUR
+    echo 'Welcome back, '(set_color --bold $MAIN_COLOUR)(whoami)'@'(hostname)(set_color normal)(set_color $TEXT_COLOUR)', ya silly 🪿💨💨💨'
+
     echo '                 '(set_color $MAIN_COLOUR)'___
   ___======____='(set_color $ACCENT_COLOUR)'-'(set_color $SECONDARY_COLOUR)'-'(set_color $ACCENT_COLOUR)'-='(set_color $MAIN_COLOUR)')     '(set_color $SECONDARY_COLOUR)''(echo $INFO_DATE)''(set_color $MAIN_COLOUR)'
 /T            \_'(set_color $SECONDARY_COLOUR)'--='(set_color $ACCENT_COLOUR)'=='(set_color $MAIN_COLOUR)')    '(set_color $SECONDARY_COLOUR)''(echo $INFO_TIME)''(set_color $MAIN_COLOUR)'
@@ -34,6 +46,6 @@ function fish_greeting
                       '(set_color $MAIN_COLOUR)'/'(set_color $ACCENT_COLOUR)'J'(set_color $SECONDARY_COLOUR)'\\'(set_color $ACCENT_COLOUR)'J'(set_color $MAIN_COLOUR)'T\\'(set_color $ACCENT_COLOUR)'JJJ'(set_color $MAIN_COLOUR)'J)
                       (J'(set_color $ACCENT_COLOUR)'JJ'(set_color $MAIN_COLOUR)'| \UUU)
                        (UU)'(set_color normal)
-  
-  echo ''
+
+    echo ''
 end
